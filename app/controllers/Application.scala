7cf4f658116c4fd4da2dce0610bd4daf9c18544c
pamu <nagarjuna.pamu@gmail.com>
2015-02-22 00:38:34 +0530

    welcome msg change

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
   def index = Action {
-    Ok(views.html.index("Welcome to Apptitude"))
+    Ok(views.html.index("Apptitude 2015"))
   }
   def contact() = Action {
     Redirect(routes.Application.index()) withSession("contact" -> "success")
