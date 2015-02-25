844d2c387b3cce8c54338eb7ca3a4ff955fd5f0a
pamu <nagarjuna.pamu@gmail.com>
2015-02-25 14:50:32 +0530

    page hits counter workig

--- public/js/utils/hits.js
+++ public/js/utils/hits.js
@@ -1,6 +1,16 @@
 $(function() {
+
+    $.get("/hits", function(data) {
+        $("#hits").text(data);
+    });
+    
     if(!!window.EventSource) {
-        var source = new EventSource("/h")
+        var source = new EventSource("/hitsStream")
+        source.addEventListener('message', function(e) {
+            var data = e.data;
+            $("#hits").text(data);
+        });
     } else {
+        $("#hits_section").html("Sorry. This browser doesn't seem to support Server sent event.Check <a href='http://html5test.com/compare/feature/communication-eventSource.html'>html5test</a> for browser compatibility.");
     }
 });
