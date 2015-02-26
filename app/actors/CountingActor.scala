34e70208ab0d7f7c778f417882a2643791472c41
pamu <nagarjuna.pamu@gmail.com>
2015-02-27 03:04:50 +0530

    counter set to 1000 intially

--- app/actors/CountingActor.scala
+++ app/actors/CountingActor.scala
@@ -16,7 +16,7 @@ class CountingActor extends Actor with ActorLogging {
   
   val (enumerator, channel) = Concurrent.broadcast[String]
   
-  var hits = BigInt(0)
+  var hits = BigInt(1000)
   
   import CountingActor._
   
