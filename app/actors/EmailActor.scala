80c9e2383f54fc0a8d5ce1c1e266d1512222b472
pamu <nagarjuna.pamu@gmail.com>
2015-02-28 02:35:33 +0530

    verbose errors

--- app/actors/EmailActor.scala
+++ app/actors/EmailActor.scala
@@ -55,7 +55,7 @@ class EmailActor extends Actor with ActorLogging {
       mailFuture pipeTo self
     }
     case failure: Failure => {
-      log.info("Sending email failed reason: {}", failure.cause)
+      log.info("Sending email failed reason: {}", failure.toString)
     }
     case Success => {
       log.info("Email sent :)")
